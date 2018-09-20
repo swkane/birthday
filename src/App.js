b224d0d8c5d3972ddcec74c8863370754ee4dce3
swkane <samuelwkane@gmail.com>
2018-09-20 19:20:11 -0400

    three popups

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import React, { Component } from "react";
-import { Message } from "semantic-ui-react";
+import { Message, Card, Container, Modal, Image } from "semantic-ui-react";
+import gift from "./gift.jpeg";
+import halfdome from "./halfdome.jpg";
+import stitchfix from "./stitchfix.jpeg";
 
 class App extends Component {
   render() {
     return (
-      <div style={styles}>
-        <Message>Happy Birthday Abby!!! 🎉 🎉 🎉</Message>
+      <div style={styles.container}>
+        <Message size="huge">Happy Birthday Abby!!! 🎉 🎉 🎉</Message>
+        <Container>
+          <Card>
+            <Modal
+              trigger={<img style={styles.image} src={gift} alt="gift1" />}
+            >
+              <Modal.Content image>
+                <Image wrapped size="medium" src={halfdome} />
+              </Modal.Content>
+            </Modal>
+          </Card>
+          <Card>
+            <Modal
+              trigger={<img style={styles.image} src={gift} alt="gift2" />}
+            >
+              <Modal.Content>Look Up</Modal.Content>
+            </Modal>
+          </Card>
+          <Card>
+            <Modal
+              trigger={<img style={styles.image} src={gift} alt="gift3" />}
+            >
+              <Modal.Content image>
+                <Image wrapped size="medium" src={stitchfix} />
+              </Modal.Content>
+            </Modal>
+          </Card>
+        </Container>
       </div>
     );
   }
 }
 
 const styles = {
-  width: "600px"
+  container: {
+    width: "370px"
+  },
+  image: {
+    width: "150px",
+    textAlign: "center"
+  }
 };
 
 export default App;
